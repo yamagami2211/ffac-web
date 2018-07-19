3b84d8bf455d7787200a58d18f6512a54d1b8904
yamagami2211 <yamagami2211@svk.jp>
2018-07-19 16:50:57 +0900

    Twittercardのリンクを変更

--- inc/header.php
+++ inc/header.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?= $title ?> / 自分を守る、みんなで守る。</title>
-        <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="/css/style.css?v=0.1">
         <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
 
         <meta name="twitter:card" content="summary_large_image">
@@ -12,7 +12,7 @@
         <meta name="twitter:creator" content="@nirot1r">
         <meta name="twitter:title" content="Fight for artistic creativity / 自分を守る、みんなで守る。">
         <meta name="twitter:description" content="#このリプ欄にはグロ画像があります 不適切な内容を投稿しているユーザから身を守る方法を提供しています。">
-        <meta name="twitter:image" content="//ffac.g-second.net/images/card.jpg">
+        <meta name="twitter:image" content="//cdn.yama2211.biz/public/ffac/card.jpg">
         <script src="//code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
 
     </head>
